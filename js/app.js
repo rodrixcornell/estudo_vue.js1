e21a5fe8a70b26d8999ce8c6fede9532dc86e619
Rodrigo Cabral <rodrixcornell@gmail.com>
2018-03-10 22:40:47 -0400

    No ramo develop Your branch is up-to-date with 'origin/develop'.
    Mudanças a serem submetidas:
    	new file:   dataServer.json
    	modified:   index.html
    	modified:   js/app.js
    	modified:   package.json

--- js/app.js
+++ js/app.js
@@ -1,63 +1,97 @@
 var app = new Vue({
   el: '#app',
   data: {
-    message: 'Hello Vue!',
-    peoples: [
-      {name:'Maria'},
-      {name:'Pedro'},
-      {name:'Gustavo'},
-      {name:'Ana'},
-      {name:'Gabriel'}
-    ],
-    newElement:'',
-    elements: [],
-    objectA: {
-      'font-size':'30px',
-      fontSize:'30px'
-    },
-    objectB: {
-      color:'red'
-    },
-    myListForm: [],
-    myForm: {
-      name:'',
-      email:''
+    books:[],
+    mySearch:'',
+    orderCol:'id',
+    orderInverse:1,
+    pagination:{
+      maxPage:5,
+      current:1,
+      totalItems:0,
+      totalPages:0,
+      listNumbers:[],
+      listPagination:[]
     }
   },
   methods: {
-    addElement: function (e) {
-      // console.log(e);
-      // e.preventDefault();
-
-      var title = this.newElement.trim();
-      if (title) {
-        this.elements.push({title:title});
-        this.newElement = '';
-      }
-    },
-
-    removeElement: function (e,index) {
+    filterOrderBy:function(e,col) {
       e.preventDefault();
-      console.log(e);
-      console.log(index);
-      this.elements.splice(index,1);
+      this.orderCol = col;
+      this.orderInverse = this.orderInverse * -1;
+      console.log(this.orderInverse);
     },
+    previous:function(e) {
+      e.preventDefault();
 
-    myClick: function () {
-      alert('click');
+      if(this.pagination.current === 1) {
+        return false;
+      }
+      this.pagination.current = this.pagination.current - 1;
+      this.books = this.pagination.listPagination[this.pagination.current - 1];
+
+      console.log('previous');
+      console.log('maxPage: ' + this.pagination.maxPage);
+      console.log('current: ' + this.pagination.current);
+      console.log('totalItems: ' + this.pagination.totalItems);
+      console.log('totalPages: ' + this.pagination.totalPages);
+      console.log('listNumbers: ' + this.pagination.listNumbers);
+      console.log('listPagination: ' + this.pagination.listPagination);
     },
+    next:function(e) {
+      e.preventDefault();
 
-    myKeyUp: function () {
-      alert('myKeyUp');
+      if(this.pagination.current === this.pagination.totalPages) {
+        return false;
+      }
+      this.pagination.current = this.pagination.current + 1;
+      this.books = this.pagination.listPagination[this.pagination.current - 1];
+
+      console.log('next');
+      console.log('maxPage: ' + this.pagination.maxPage);
+      console.log('current: ' + this.pagination.current);
+      console.log('totalItems: ' + this.pagination.totalItems);
+      console.log('totalPages: ' + this.pagination.totalPages);
+      console.log('listNumbers: ' + this.pagination.listNumbers);
+      console.log('listPagination: ' + this.pagination.listPagination);
     },
+    pagePagination:function(e,current){
+      e.preventDefault();
 
-    addForm: function () {
-      this.myListForm.push({
-        name:this.myForm.name,
-        email:this.myForm.email
-      });
-      this.myForm.name = '';
-      this.myForm.email = '';
+      this.pagination.current = current + 1;
+      this.books = this.pagination.listPagination[current];
+
+      console.log('pagePagination: ' + current);
+      console.log('maxPage: ' + this.pagination.maxPage);
+      console.log('current: ' + this.pagination.current);
+      console.log('totalItems: ' + this.pagination.totalItems);
+      console.log('totalPages: ' + this.pagination.totalPages);
+      console.log('listNumbers: ' + this.pagination.listNumbers);
+      console.log('listPagination: ' + this.pagination.listPagination);
     }
+  },
+  ready: function() {
+    var self = this;
+    self.$http.get('dataServer.json').then(function(response) {
+      console.log(response);
+      self.pagination.totalItems = response.data.length;
+      self.pagination.totalPages = Math.ceil(response.data.length / self.pagination.maxPage);
+
+      var aux = [];
+      for(var k in response.data) {
+        aux.push(response.data[k]);
+        if(aux.length === self.pagination.maxPage) {
+          self.pagination.listPagination.push(aux);
+          aux = [];
+        }
+      }
+      if(aux.length > 0) {
+        self.pagination.listPagination.push(aux);
+      }
+      console.log(self.pagination.listPagination);
+
+      // self.books = response.data;
+      self.books = self.pagination.listPagination[0];
+    });
   }
 })
